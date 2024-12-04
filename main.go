28571c9bb5f6c57f1341620835adaa22566be6c6
Gunth15 <cjw041@email.latech.edu>
2024-12-03 18:29:30 -0600

    Started working woth go's standard lib templating engine

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/chat_app/context"
+
+	_ "github.com/mattn/go-sqlite3"
+)
 
 /*
 Need these routes:
 - landing page/ GET
-- signup page/ GET, POST
-- login page/ GET, POST
-- update login page/ GET, PUT
 - logout button / Remove cookie
 - profile page/ GET(should be able to see current chat rooms)
 - page so users can find each other(Look to create new rooms)
@@ -18,5 +25,31 @@ How does one use cookies?
 */
 
 func main() {
-	fmt.Println("Start project lol")
+	db, err := sql.Open("sqlite3", "./database/chat_app.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tmpl, err := template.ParseFS(os.DirFS("./templates/"), "*.html", "*/*.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctxt := context.Ctxt{
+		Db:   db,
+		Tmpl: tmpl,
+	}
+
+	http.HandleFunc("GET /login/", ctxt.GetLogin)
+	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		ctxt.Tmpl.ExecuteTemplate(w, "landing.html", nil)
+	})
+	http.HandleFunc("GET /signup/", ctxt.GetSignup)
+	http.HandleFunc("GET /update/", ctxt.GetUpdateLogin)
+	http.HandleFunc("GET /profile/", ctxt.GetProfile)
+
+	port := ":8080"
+	log.Printf("Running on localhost%s", port)
+	defer log.Print("Shutting Down")
+	log.Fatal(http.ListenAndServe(port, nil))
 }
